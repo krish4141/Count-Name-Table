8e5319b641179161e8bd5942a17440f25e59bd0b
krishna vishwanath bhashinge <bhashinge.vishwanath@mphasis.com>
2023-10-19 20:26:15 +0530

    project added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [butttonClick, setButtonClick] = useState(false);
+  const [nameData, setNameData] = useState([]);
+
+  const handleClick = () => {
+    const jsonData = ['krishna', 'ram', 'raj', 'krishna', 'ram', 'krishna'];
+
+    const nameCount = jsonData.reduce((count, name) => {
+      count[name] = (count[name] || 0) + 1;
+      return count;
+    }, {});
+
+    const data = Object.entries(nameCount).map(([name, count]) => ({
+      name,
+      count,
+    }));
+
+    setNameData(data);
+    setButtonClick(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Name Counts</h1>
+      <button onClick={handleClick}>Click to the Name Counts</button>
+      {butttonClick && (
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Count</th>
+            </tr>
+          </thead>
+          <tbody>
+            {nameData.map((item, index) => (
+              <tr key={index}>
+                <td>{item.name}</td>
+                <td>{item.count}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
